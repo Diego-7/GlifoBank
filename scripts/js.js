bd3dccb94faf3abda1b6454b1eb70c2d29b5c7ba
Diego Ramos <diego_ramos93@outlook.com>
2021-11-06 13:23:52 -0300

    sepração do script

---
+++ scripts/js.js
@@ -0,0 +1,17 @@
+var js_email;
+				var js_senha;
+
+				window.alert("Coletamos cookies para melhor te atender");
+
+				function login(){
+					js_email= prompt("E-mail")
+					js_senha= prompt("Senha")
+
+					switch (parseInt(js_senha)) {
+
+						case 1234 : window.alert("Login Aprovado"); break;
+						case 4321 : window.alert('Login Aprovado'); break;
+						case 54321 : window.alert("Login aprovado"); break;
+						default : window.alert("Senha Incorreta!"); break;
+					}
+				}
