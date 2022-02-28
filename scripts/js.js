7cfee83f71e76f396978ccc3aff6392685e4b923
Diego Ramos <diego_ramos93@outlook.com>
2022-02-27 22:55:47 -0300

    mudanca geral

--- scripts/js.js
+++ scripts/js.js
@@ -1,9 +1,43 @@
-/*Cookie Advice Bar*/
-
+//Cookie Advice Bar
 function hideCookieBox(){
     
 let cookieHide = document.querySelector('.cookie_advice');
 cookieHide.style.display = 'none'
 }
+
+//login home page
+const loginDiv = document.querySelector(".login");
+const btnLogin = document.querySelector(".loginBtn");
+const closeBtn = document.querySelector('.loginClose');
+
+function logIn(){
+
+    loginDiv.style.display = 'block';
+
+    btnLogin.style.display = 'none';
+
+    closeBtn.style.display = 'block';
+
+}
+
+const closeLogin = document.querySelector(".loginClose");
+
+//close login div button
+closeLogin.addEventListener('click', () =>{
+    if(closeLogin.click){
+
+        loginDiv.style.display = 'none';
+
+        closeBtn.style.display = 'none';
+
+        btnLogin.style.display = 'block';
+    }
+})
+
+
+
+
+
+
 
 
